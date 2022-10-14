7514b324a9604178b4dce274224fae6c8d158cbe
shehroz kapoor <shehrozkapoor@gmail.com>
2022-10-14 10:08:01 +0500

    feature/Product Goal: As a Product Owner, I would like to build a Microservices API Web Application for 'Delivery Services (Unit Tests)

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Voosool 1233'),
+      home: const MyHomePage(title: 'splash screen'),
     );
   }
 }
