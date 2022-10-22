0e9de83df6ce2bfca1dde279fe2db61cb3a176c7
Tahir Siddique <tahircreativedev577@gmail.com>
2022-10-22 18:04:02 +0500

    As a Developer, I would like to build order status page same as UI/UX.( user can track their order.)

--- lib/screens/mobile_view/delivery_process.dart
+++ lib/screens/mobile_view/delivery_process.dart
@@ -1,15 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:voosool_flutter/theme.dart';
 
 class DeliveryProcess extends StatelessWidget {
   const DeliveryProcess({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var size2 = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: primary,
       appBar: AppBar(
-        title: Text('DeliveryProcess'),
+        actions: [
+          Icon(Icons.screen_share),
+          Icon(Icons.card_giftcard),
+        ],
+        backgroundColor: primary,
+        elevation: 0,
       ),
-      body: Center(
-        child: Text('DeliveryProcess'),
+      body: SingleChildScrollView(
+        child: Container(
+          width: size2.width,
+          margin: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  "Keep eye on your Order.",
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Preparing your food...",
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 50,
+                ),
+                child: Text(
+                  "Delivering...",
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 10,
+                      width: size2.width * .15,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: size2.width * .7,
+                margin: EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () => {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.black),
+                  ),
+                  child: Text(
+                    "Give us Feedback",
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
